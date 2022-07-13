91ba21afc57eb46b21f131ef35b2933335570bd3
HSCnoKenju <26322756+HSCnoKenju@users.noreply.github.com>
2022-07-13 22:18:23 +0200

    resources pathplanner and ignore generated map

---
+++ PathPlanner/resources/userContinue.kt
@@ -0,0 +1,31 @@
+import it.unibo.kactor.ActorBasic
+import kotlinx.coroutines.delay
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+object userContinue {
+
+
+    suspend fun  userEnd(owner:ActorBasic){
+
+//
+//        var br = BufferedReader(InputStreamReader(System.`in`))
+//
+//        println("${owner.name} | press enter to continue ...")
+//        try
+//        {
+//            while (! (br.readLine().equals("\n"))){
+//
+//            }
+//
+//
+//        }catch (  e:java.io.IOException)
+//        {}
+//        println("${owner.name} | continue")
+
+        delay(1000)
+    }
+
+    
+
+}
