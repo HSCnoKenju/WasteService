020b09f843ca2442182024d5bd228088ca0bfec7
HSCnoKenju <26322756+HSCnoKenju@users.noreply.github.com>
2022-08-23 17:05:57 +0200

    sprint2 full test without led and sonar

--- CoreWasteService/waste_service.pl
+++ CoreWasteService/waste_service.pl
@@ -1,10 +1,7 @@
 %====================================================================================
 % waste_service description   
 %====================================================================================
-context(ctxpathplanner, "127.0.0.1",  "TCP", "8078").
+context(ctxpathexec, "127.0.0.1",  "TCP", "8049").
 context(ctxwaste, "localhost",  "TCP", "8033").
- qactor( pathplanner, ctxpathplanner, "external").
-  qactor( transporttrolley, ctxwaste, "it.unibo.transporttrolley.Transporttrolley").
-  qactor( waste_truck_mock, ctxwaste, "it.unibo.waste_truck_mock.Waste_truck_mock").
-  qactor( waste_service, ctxwaste, "it.unibo.waste_service.Waste_service").
-tracing.
+ qactor( pathexec, ctxpathexec, "external").
+  qactor( mocksystem, ctxwaste, "it.unibo.mocksystem.Mocksystem").
--- launchall_test.sh
+++ launchall_test.sh
@@ -5,4 +5,4 @@
 
 gnome-terminal --title=Wenv -- sh -c 'cd BasicRobot ; docker-compose -f virtualRobotOnly4.0.yaml up'
 gnome-terminal --title=Basicrobot -- sh -c 'cd BasicRobot ; ./gradlew run; read ONE' 
-
+gnome-terminal --title=Coreservice -- sh -c 'cd CoreWasteService ; ./gradlew run ; read ONE'
