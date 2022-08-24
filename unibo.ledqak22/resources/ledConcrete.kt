9c6c46f6ebc79f63a5c1ff1ae50a3fe68ba0622d
HSCnoKenju <26322756+HSCnoKenju@users.noreply.github.com>
2022-08-24 17:04:39 +0200

    ledqak with concrete and simulation

---
+++ unibo.ledqak22/resources/ledConcrete.kt
@@ -0,0 +1,49 @@
+import it.unibo.kactor.ActorBasic
+import it.unibo.kactor.ApplMessage
+import it.unibo.kactor.IApplMessage
+import kotlinx.coroutines.delay
+import it.unibo.kactor.MsgUtil
+import kotlinx.coroutines.runBlocking
+import java.lang.Exception
+
+class ledConcrete (name: String) : ActorBasic(name) {
+
+
+    override suspend fun actorBody(msg: IApplMessage) {
+
+        println("$tt $name | received  $msg "  )  //RICEVE GLI EVENTI!!!
+
+        if(msg.msgId() == "ledon") {
+
+
+            try {
+                Runtime.getRuntime().exec("sudo bash led25GpioTurnOn.sh")
+
+            }catch (e : Exception){
+                println("WARNING: ledConcrete does not find led25GpioTurnOn.sh")
+            }
+
+
+        } else if (msg.msgId() == "ledblink") {
+
+            try {
+                Runtime.getRuntime().exec("sudo bash led25GpioBlink.sh")
+
+            }catch (e : Exception){
+                println("WARNING: ledConcrete does not find led25GpioBlink.sh")
+            }
+
+        }
+        else /* led off*/ {
+
+            try {
+                Runtime.getRuntime().exec("sudo bash led25GpioTurnOff.sh")
+
+            }catch (e : Exception){
+                println("WARNING: ledConcrete does not find led25GpioTurnOff.sh")
+            }
+
+        }
+
+    }
+}
---
+++ unibo.ledqak22/resources/ledSimulator.kt
@@ -0,0 +1,37 @@
+import it.unibo.kactor.ActorBasic
+import it.unibo.kactor.ApplMessage
+import it.unibo.kactor.IApplMessage
+import kotlinx.coroutines.delay
+import it.unibo.kactor.MsgUtil
+import kotlinx.coroutines.runBlocking
+
+class ledSimulator (name :String) : ActorBasic (name) {
+
+    override suspend fun actorBody(msg: IApplMessage) {
+        println("$tt $name | received  $msg "  )  //RICEVE GLI EVENTI!!!
+
+        if(msg.msgId() == "ledon") {
+
+            unibo.actor22comm.utils.ColorsOut.outappl(
+                "$name - on", unibo.actor22comm.utils.ColorsOut.MAGENTA)
+
+        } else if (msg.msgId() == "ledblink") {
+
+            unibo.actor22comm.utils.ColorsOut.outappl(
+                "$name - blink", unibo.actor22comm.utils.ColorsOut.MAGENTA)
+
+
+        }
+        else /* led off*/ {
+
+            unibo.actor22comm.utils.ColorsOut.outappl(
+                "$name - off", unibo.actor22comm.utils.ColorsOut.MAGENTA)
+
+
+        }
+
+
+    }
+
+
+}
