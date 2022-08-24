1e50984fffe424a816bd63e27b08f8db96a85a89
HSCnoKenju <26322756+HSCnoKenju@users.noreply.github.com>
2022-08-24 17:03:58 +0200

    rename event sonar into sonardata to match old sonarqak

--- CoreWasteService/waste_service.pl
+++ CoreWasteService/waste_service.pl
@@ -1,7 +1,7 @@
 %====================================================================================
 % waste_service description   
 %====================================================================================
+mqttBroker("broker.hivemq.com", "1883", "unibo/hu/waste").
 context(ctxwaste, "localhost",  "TCP", "8033").
- qactor( thresholdchecker, ctxwaste, "it.unibo.thresholdchecker.Thresholdchecker").
-  qactor( sonarmockalternate, ctxwaste, "it.unibo.sonarmockalternate.Sonarmockalternate").
-  qactor( mocktrolleylistener, ctxwaste, "it.unibo.mocktrolleylistener.Mocktrolleylistener").
+ qactor( trolleyanalyzer, ctxwaste, "it.unibo.trolleyanalyzer.Trolleyanalyzer").
+  qactor( trolleymock, ctxwaste, "it.unibo.trolleymock.Trolleymock").
