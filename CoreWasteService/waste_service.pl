345195311f26eebce7ffbc7b5e5e29693d59e6f1
HSCnoKenju <26322756+HSCnoKenju@users.noreply.github.com>
2022-09-22 14:21:58 +0200

    fix naming

--- CoreWasteService/waste_service.pl
+++ CoreWasteService/waste_service.pl
@@ -2,6 +2,9 @@
 % waste_service description   
 %====================================================================================
 mqttBroker("broker.hivemq.com", "1883", "unibo/hu/waste").
+context(ctxpathexec, "127.0.0.1",  "TCP", "8049").
 context(ctxwaste, "localhost",  "TCP", "8033").
- qactor( trolleyanalyzer, ctxwaste, "it.unibo.trolleyanalyzer.Trolleyanalyzer").
-  qactor( trolleymock, ctxwaste, "it.unibo.trolleymock.Trolleymock").
+ qactor( pathexec, ctxpathexec, "external").
+  qactor( thresholdchecker, ctxwaste, "it.unibo.thresholdchecker.Thresholdchecker").
+  qactor( mocksystem, ctxwaste, "it.unibo.mocksystem.Mocksystem").
+  qactor( trolleyanalyzer, ctxwaste, "it.unibo.trolleyanalyzer.Trolleyanalyzer").
