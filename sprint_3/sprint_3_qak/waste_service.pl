3288820408b3e817dd795e2f30aa1b385076fc63
HSCnoKenju <26322756+HSCnoKenju@users.noreply.github.com>
2022-10-05 14:19:50 +0200

    refactor : sprint 3

---
+++ sprint_3/sprint_3_qak/waste_service.pl
@@ -0,0 +1,28 @@
+%====================================================================================
+% waste_service description   
+%====================================================================================
+context(ctxbasicrobot, "localhost",  "TCP", "8020").
+context(ctxpathexec, "localhost",  "TCP", "8043").
+context(ctxpathplanner, "localhost",  "TCP", "8078").
+context(ctxconfig, "localhost",  "TCP", "8034").
+context(ctxtest, "localhost",  "TCP", "8035").
+context(ctxwaste, "localhost",  "TCP", "8033").
+context(ctxledqak22, "localhost",  "TCP", "8065").
+context(ctxgui, "localhost",  "TCP", "8082").
+context(ctxtruck, "localhsot",  "TCP", "8083").
+context(ctxsonarqak22, "localhost",  "TCP", "8061").
+ qactor( gui, ctxgui, "it.unibo.gui.Gui").
+  qactor( trolleyanalyzer, ctxwaste, "it.unibo.trolleyanalyzer.Trolleyanalyzer").
+  qactor( thresholdchecker, ctxwaste, "it.unibo.thresholdchecker.Thresholdchecker").
+  qactor( sonar, ctxsonarqak22, "it.unibo.sonar.Sonar").
+  qactor( led, ctxledqak22, "it.unibo.led.Led").
+  qactor( pathexec, ctxpathexec, "it.unibo.pathexec.Pathexec").
+  qactor( basicrobot, ctxbasicrobot, "it.unibo.basicrobot.Basicrobot").
+  qactor( pathplanner, ctxpathplanner, "it.unibo.pathplanner.Pathplanner").
+  qactor( boundarymapbuilder, ctxpathplanner, "it.unibo.boundarymapbuilder.Boundarymapbuilder").
+  qactor( configurer, ctxconfig, "it.unibo.configurer.Configurer").
+  qactor( tester, ctxtest, "it.unibo.tester.Tester").
+  qactor( transporttrolley, ctxwaste, "it.unibo.transporttrolley.Transporttrolley").
+  qactor( waste_truck_mock, ctxtruck, "it.unibo.waste_truck_mock.Waste_truck_mock").
+  qactor( waste_service, ctxwaste, "it.unibo.waste_service.Waste_service").
+tracing.
